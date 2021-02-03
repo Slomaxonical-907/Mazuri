890eba2f93fe8ab449685c1181414ebc4ae7c7a0
Slomaxonical-907 <Slomaxonical907@gmail.com>
2021-02-03 22:53:37 +0200

    Day0

---
+++ src/main/java/com/slomaxonical/mazuri/Mazuri.java
@@ -0,0 +1,11 @@
+package com.slomaxonical.mazuri;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mazuri implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
